8227f365a5ada2d628abace37360563574f7ee7a
LUCAS SOUZA MARINHO <45193262813@info.local>
2026-04-16 16:15:04 -0300

    Fake API e fim da validacao

--- Contact Form/js/script.js
+++ Contact Form/js/script.js
@@ -1,4 +1,6 @@
+
 function validarFormulario() {
+
     let nome = document.getElementById("Nome").value;
     let sobrenome = document.getElementById("Sobrenome").value;
     let email = document.getElementById("Email").value;
@@ -8,12 +10,12 @@ function validarFormulario() {
     let Cep = document.getElementById("Phone").value;
     let Rua = document.getElementById("Rua").value;
     let Numero = document.getElementById("Numero").value;
+    let Bairro = document.getElementById("Bairro").value;
+    let Cidade = document.getElementById("Cidade").value;
+    let Estado = document.getElementById("Estado").value;
     let Complemento = document.getElementById("Complemento").value;
-    let Bairro = document.getElementById("Complemento").value;
-    let Cidade = document.getElementById("Complemento").value;
-    let Estado = document.getElementById("Complemento").value;
     let Anotacoes = document.getElementById("Anotacoes").value;
-
+    
     let quantidadeErros = 0;
 
     //Nome
@@ -71,7 +73,7 @@ function validarFormulario() {
     else {
         reiniciaBorda("Phone")
     }
-    
+
     //Cep
     if (Cep.trim().length == 0) {
         formError("Cep");
@@ -99,15 +101,7 @@ function validarFormulario() {
         reiniciaBorda("Numero")
     }
 
-    //Complemento
-    if (Complemento.trim().length == 0) {
-        formError("Complemento");
-        quantidadeErros++;
-    }
-    else {
-        reiniciaBorda("Complemento")
-    }
-    
+
     //Bairro
     if (Bairro.trim().length == 0) {
         formError("Bairro");
@@ -131,25 +125,39 @@ function validarFormulario() {
         formError("Estado");
         quantidadeErros++;
     }
-
-    //Anotacoes
-    if (Anotacoes.trim().length == 0) {
-        formError("Anotacoes");
-        quantidadeErros++;
-    }
     else {
-        reiniciaBorda("Anotacoes")
+        reiniciaBorda("Estado")
     }
 
     //Verifica
     if (quantidadeErros != 0) {
         alert("Existem " + quantidadeErros + " erros no formúlario")
+        document.getElementById("Corpo").style.backgroundImage = "red"
         quantidadeErros = 0;
     }
     else {
-        alert("Formulário enviado com sucesso");
-        reiniciaTodasAsBordas();
+        let objetoContato = {
+            Nome: nome,
+            Sobrenome: sobrenome,
+            Email: email,
+            DDI: DDI,
+            DDD: DDD,
+            Phone: Phone,
+            Cep: Cep,
+            Rua: Rua,
+            Numero: Numero,
+            Bairro: Bairro,
+            Cidade: Cidade,
+            Estado: Estado,
+            Complemento: Complemento,
+            Anotacoes: Anotacoes
+        }
+
+
+        let cadastrado = cadastrarContato(objetoContato);
+        alert("Cadastrado com Sucesso")
     }
+
 }
 
 
@@ -161,4 +169,56 @@ function formError(idCampo) {
 function reiniciaBorda(idCampo) {
     document.getElementById(idCampo).style.border = "1px solid rgb(172, 172, 172)"
     document.getElementById(idCampo).style.boxShadow = "0 0 0 0"
+}
+
+async function buscarEndereco(Cep) {
+    //Async - Não sincrono, usado quando precissamos esperar uma resposta para continuar
+
+    if (Cep.trim().length < 8) {
+        alert("Cep incompleto")
+        return false
+    }
+
+    // buscar o Cep la na ViaCep
+    try {
+
+        aguardandoCampos()
+        //literal templates
+        let retorno = await fetch(`http://viacep.com.br/ws/${Cep}/json/`);
+        let dados = await retorno.json();
+
+
+       
+        document.getElementById("Rua").value = dados.logradouro;
+        document.getElementById("Bairro").value = dados.bairro;
+        document.getElementById("Complemento").value = dados.complemento;
+        document.getElementById("Cidade").value = dados.localidade;
+        document.getElementById("Estado").value = dados.uf;
+        document.getElementById("DDD").value = dados.ddd;
+    }
+    catch {
+        alert("Erro ao procurar CEP")
+    }
+
+}
+
+
+function aguardandoCampos()
+{
+        document.getElementById("Rua").style.value = "...";
+        document.getElementById("Bairro").value = "...";
+        document.getElementById("Complemento").value = "...";
+        document.getElementById("Cidade").value = "...";
+        document.getElementById("Estado").value = "...";
+        document.getElementById("DDD").value = "...";
+}
+
+async function cadastrarContato(objetoContato)
+{
+  console.log(objetoContato)
+
+    const resposta = await fetch("http://localhost:3000/contatos", {
+    method: "POST",
+    body: JSON.stringify(objetoContato)
+  })
 }
